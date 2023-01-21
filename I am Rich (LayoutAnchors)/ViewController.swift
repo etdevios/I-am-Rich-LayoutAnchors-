7452a1bdd1c7e75a3bd1786cbbe963040819bd42
Eduard Tokarev <etdevios@gmail.com>
2023-01-21 22:18:44 +0300

    fix: layout label

--- I am Rich (LayoutAnchors)/ViewController.swift
+++ I am Rich (LayoutAnchors)/ViewController.swift
@@ -9,10 +9,12 @@ import UIKit
 
 final class ViewController: UIViewController {
     
+    private let mainStackView = UIStackView()
     private let label = UILabel()
+    private let bottomPaddingView = UIView()
     private let imageView = UIImageView()
     private let backgroundColor = UIColor(red: 36/255, green: 73/255, blue: 94/255, alpha: 1)
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = backgroundColor
@@ -29,21 +31,48 @@ final class ViewController: UIViewController {
         applyStyleLabel(label)
         applyStyleImageView(imageView)
         
-        [label, imageView].forEach() { item in
+        arrangeStackView(
+            mainStackView,
+            subviews: [
+                label,
+                bottomPaddingView
+            ])
+        
+        [imageView, mainStackView].forEach() { item in
             view.addSubview(item)
             item.translatesAutoresizingMaskIntoConstraints = false
         }
         
         NSLayoutConstraint.activate([
+            mainStackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            mainStackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            mainStackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            mainStackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            
             imageView.centerYAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerYAnchor),
             imageView.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
             imageView.widthAnchor.constraint(equalToConstant: 270),
             imageView.heightAnchor.constraint(equalToConstant: 270),
-            
-            label.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
-            label.bottomAnchor.constraint(equalTo: imageView.topAnchor, constant: -70)
-            
         ])
+    }
+    
+    func arrangeStackView(
+        _ stackView: UIStackView,
+        subviews: [UIView],
+        spacing: CGFloat = 0,
+        axis: NSLayoutConstraint.Axis = .vertical,
+        distribution: UIStackView.Distribution = .fillEqually,
+        alignment: UIStackView.Alignment = .center
+    ){
+        stackView.axis = axis
+        stackView.spacing = spacing
+        stackView.distribution = distribution
+        stackView.alignment = alignment
+        
+        subviews.forEach { item in
+            item.translatesAutoresizingMaskIntoConstraints = false
+            stackView.addArrangedSubview(item)
+        }
     }
     
     func applyStyleLabel(
